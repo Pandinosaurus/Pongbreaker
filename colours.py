91c1d821418f44f15f24fdc77ff616365669438b
Rémi Ratajczak <Remi.Ratajczak@gmail.com>
2017-09-26 11:12:11 +0200

    Initial commit

---
+++ colours.py
@@ -0,0 +1,19 @@
+#Project : Flying-Seal
+#File	 : colours.py
+#Author  : Remi Ratajczak
+#Contact : Remi.Ratajczak@gmail.com
+
+black   = (  0,  0,  0)
+white   = (255,255,255)
+red     = (255,  0,  0)
+blue    = (  0,  0,255)
+yellow  = (255,255,  0)
+cyan    = (  0,255,255)
+green   = (  0,128,  0)
+purple  = (128,  0,128)
+grey    = (128,128,128)
+maroon  = (128,  0,  0)
+pink    = (255,105,180)
+silver  = (192,192,192)
+brown   = (139, 69, 19)
+skyblue = (  0,191,255)
